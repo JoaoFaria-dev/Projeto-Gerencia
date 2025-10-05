dd421bea5583562cf15c8a5632642ebd23dd4853
jmaf4r1a-dev <joao.mfaria0211@gmail.com>
2025-10-05 10:50:20 -0300

    fix: resolver conflito Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,8 @@
 public class Calculadora {
     public static void main(String[] args) {
         //teste
+        System.out.println("Soma: " + Soma.somar(2,3));
+        //teste
         System.out.println("Divisão: " + Divisao.dividir(10,2));
     }
 }
