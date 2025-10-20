6a4581c0614fc18333ed2bf954f88f5d24d7d68d
jmaf4r1a-dev <joao.mfaria0211@gmail.com>
2025-10-20 10:03:29 -0300

    Teste 2

--- src/Teste.java
+++ src/Teste.java
@@ -1,5 +1,7 @@
 public class Teste {
     public static void main(String[] args) {
         System.out.println("Teste");
+        System.out.println(Soma.somar(2,3));
+        System.out.println(Soma.somar(3,4));
     }
 }
