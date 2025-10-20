6ab2c14489754f0af3781ee10162ca47671f7fca
LeticiaOMF <leticiaomf2017@icloud.com>
2025-10-20 10:20:38 -0300

    Testando mais para atividade

--- src/Testando.java
+++ src/Testando.java
@@ -2,5 +2,7 @@ public class Testando {
     public static void main(String[] args) {
         System.out.println("oii");
         System.out.println("Texugos");
+        System.out.println("mais Texugos");
+        System.out.println("Kiwis");
     }
 }
