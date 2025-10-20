4e3af4e1c11a95d6d0615e7f186b79f7ccab4ca1
LeticiaOMF <leticiaomf2017@icloud.com>
2025-10-20 10:22:52 -0300

    Apaga testando

--- src/Testando.java
+++
@@ -1,9 +0,0 @@
-public class Testando {
-    public static void main(String[] args) {
-        System.out.println("oii");
-        System.out.println("Texugos");
-        System.out.println("mais Texugos");
-        System.out.println("Kiwis");
-        System.out.println("paia");
-    }
-}
