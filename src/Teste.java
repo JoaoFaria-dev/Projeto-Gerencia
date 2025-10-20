f633967ec4c2c0f1e4419704d1e5793fbcc63671
jmaf4r1a-dev <joao.mfaria0211@gmail.com>
2025-10-20 10:10:46 -0300

    Apaguei2

--- src/Teste.java
+++
@@ -1,7 +0,0 @@
-public class Teste {
-    public static void main(String[] args) {
-        System.out.println("Teste");
-        System.out.println(Soma.somar(2,3));
-        System.out.println(Soma.somar(3,4));
-    }
-}
