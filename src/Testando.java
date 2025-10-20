6cb8385f99b141f0c30114d4b1eca8fc744a1a52
LeticiaOMF <leticiaomf2017@icloud.com>
2025-10-20 10:24:15 -0300

    Usando revert

---
+++ src/Testando.java
@@ -0,0 +1,8 @@
+public class Testando {
+    public static void main(String[] args) {
+        System.out.println("oii");
+        System.out.println("Texugos");
+        System.out.println("mais Texugos");
+        System.out.println("Kiwis");
+    }
+}
